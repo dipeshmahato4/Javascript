090d2fddf5f55e3038fb93ecccb63d48394f57e4
Dipesh Mahato <mahatodipesh10@gmail.com>
2023-05-22 13:01:52 +0545

    update

--- lesson11/todo-list.js
+++ lesson11/todo-list.js
@@ -1,5 +1,20 @@
 
-const ToDoList1=[];
+const ToDoList1=['wash dishes', 'make dinner'];
+
+rendertodolist();
+function rendertodolist(){
+    let htmlcode='';// variable to store the result
+
+    for(let i =0 ; i<ToDoList1.length; i++){
+      const Todo =ToDoList1[i];
+      const html = `<p>${Todo}</p>`;
+      // to combine 
+      htmlcode += html
+    }
+    console.log(htmlcode);
+
+document.querySelector('.todolist').innerHTML=htmlcode;
+}
 
 function addButton(){
  const inputElement= document.querySelector('.userInput');
@@ -9,7 +24,7 @@ function addButton(){
 
   ToDoList1.push(name);
   console.log(ToDoList1);
+  inputElement.value=''; 
 
-  inputElement.value='';
-  
+  rendertodolist();
 }
